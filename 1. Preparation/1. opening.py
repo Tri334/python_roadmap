35928ead516013404c32b56abbb2108ccaf544b3
Dewa Gede <50865141+Tri334@users.noreply.github.com>
2023-09-02 22:27:47 +0800

    Opening Python
    - Hint to start python

---
+++ 1. Preparation/1. opening.py
@@ -0,0 +1,10 @@
+"""
+Selamat datang di dasar pemrograman Python
+Hal yang pertama kali harus dilakukan adalah 
+- Mulailah belajar dasar dasar mengikuti roadmap, contoh : roadmap.sh -> ikuti python
+- Download python, contoh saya akan gunakan python versi terbaru 3.11
+- Set Path ( biasanya auto tetapi jika error di set path maka tanya chat GPT)
+- Set Environtment, contoh: python -m venv trainingEnv
+- Taruh folder environment di .gitignore
+- Jangan lupa aktifkan env nya (Bingung tanya chat GPT)
+"""
