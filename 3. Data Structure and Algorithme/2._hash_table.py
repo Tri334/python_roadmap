3b706575a112712423d91ffce32b42c021ac2da6
Dewa Gede <50865141+Tri334@users.noreply.github.com>
2023-09-16 22:41:59 +0800

    Update 2._hash_table.py

--- 3. Data Structure and Algorithme/2._hash_table.py
+++ 3. Data Structure and Algorithme/2._hash_table.py
@@ -5,16 +5,18 @@ hash_data = {}
 '''Buatlah harga jual sayur dan buah berdasarkan jenis nya'''
 
 data_harga = hash_data
-
+'''Tambah data'''
 data_harga['wortel'] = 30
 data_harga['semangka'] = 300
 data_harga['manggis'] = 900
 
 print(data_harga)
 print('==================')
+'''Hilangkan data semangka'''
 del data_harga['semangka']
 print(data_harga)
 
+'''Check apakah sayur/buah ada di variable'''
 print('==================')
 data_anggur = data_harga.get('anggur', False)  # If 'anggur' is not in the hash map, it returns False
 print(data_anggur)
